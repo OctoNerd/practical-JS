84b47782eccfb02c07baba4e1fe677eb99a86a81
Nick Eddy <nick.a.eddy@gmail.com>
2016-07-15 11:21:47 -0700

    added toggleCompleted method to todoList object

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 
 var todoList = {
-	//Stores todo list items on an array
+	// Stores todo list items on an array
 	todos: [],
-	//Displays todos to console:
+	// Displays todos to console
 	showTodos: function() {
 		console.log('todos array: ', this.todos);
 	},
-	//Builds a new todo object with Todo constructor and adds it to the array:
+	// Builds a new todo object with Todo constructor and adds it to the array
 	newTodo: function(text) {
 		this.todos.push({
 			text: text,
@@ -14,12 +14,18 @@ var todoList = {
 		});
 		this.showTodos();
 	},
-	//Edits the text of a todo by array index number:
+	// Edits the text of a todo by array index number
 	editTodo: function(i, newText) {
 		this.todos[i].text = newText;
 		this.showTodos();
 	},
-	//remove one todo at index
+	// Toggles the completed value of a todo object at (position 'i') between true and false
+	toggleCompleted: function(i) {
+		var todo = this.todos[i];
+		todo.completed = !todo.completed;
+		this.showTodos();
+	},
+	// Remove one todo at index position 'i'
 	deleteTodo: function(i) {
 		this.todos.splice(i, 1);
 		this.showTodos();
